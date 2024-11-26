0c57690f953a38ffe46effa9b9566d89a8d09c5e
NguyenThanhHuyPM77 <thanhhuypm77@gmail.com>
2024-11-26 17:13:36 +0700

    Update control chieu 26/11

---
+++ src/main/java/control/DetailControl.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import dao.DAO;
+import entity.Account;
+import entity.Category;
+import entity.Product;
+import entity.Review;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(name = "DetailControl", urlPatterns = {"/detail"})
+public class DetailControl extends HttpServlet {
+
+   
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String id = request.getParameter("pid");
+        DAO dao = new DAO();
+        Product p = dao.getProductByID(id);
+        
+        int cateIDProductDetail = dao.getCateIDByProductID(id);
+        List<Product> listRelatedProduct = dao.getRelatedProduct(cateIDProductDetail);
+        
+        List<Review> listAllReview = dao.getAllReviewByProductID(id);
+        int countAllReview = listAllReview.size();
+        
+        List<Account> listAllAcount = dao.getAllAccount();
+        
+        Product last = dao.getLast();
+
+        request.setAttribute("detail", p);
+        request.setAttribute("listRelatedProduct", listRelatedProduct);
+        request.setAttribute("listAllReview", listAllReview);
+        request.setAttribute("listAllAcount", listAllAcount);
+        request.setAttribute("countAllReview", countAllReview);
+        request.setAttribute("p", last);
+        request.getRequestDispatcher("DetailProduct.jsp").forward(request, response);
+    }
+
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+   
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/main/java/control/EditControl.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import dao.DAO;
+import entity.Account;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet(name = "EditControl", urlPatterns = {"/edit"})
+public class EditControl extends HttpServlet {
+
+  
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
+        String pid = request.getParameter("id");
+        String pname = request.getParameter("name");
+        String pimage = request.getParameter("image");
+        
+        String pimage2 = request.getParameter("image2");
+        String pimage3 = request.getParameter("image3");
+        String pimage4 = request.getParameter("image4");
+        String pmodel = request.getParameter("model");
+        String pcolor = request.getParameter("color");
+        String pdelivery = request.getParameter("delivery");
+        
+        String pprice = request.getParameter("price");
+        String ptitle = request.getParameter("title");
+        String pdescription = request.getParameter("description");
+        String pcategory = request.getParameter("category");
+        DAO dao = new DAO();
+        dao.editProduct(pname, pimage, pprice, ptitle, pdescription, pcategory, pmodel, pcolor, pdelivery, pimage2, pimage3, pimage4, pid);
+       request.setAttribute("mess", "Edited!");
+       request.getRequestDispatcher("manager").forward(request, response);
+//        response.sendRedirect("manager");
+    }
+
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+   
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
